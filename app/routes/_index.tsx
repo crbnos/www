a5d99eae2bde2260fb425eef0e95ea949574ac66
Brad Barbin <bradbarbin@protonmail.com>
2025-10-30 14:51:54 -0400

    style: update headline size on mobile

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -90,7 +90,7 @@ function Hero() {
         <div className="container mx-auto px-4 relative z-10">
           <div className="flex flex-col justify-start items-center gap-8">
             <DiscordButton />
-            <h2 className="font-display text-foreground text-balance mx-auto  max-w-5xl text-center font-medium tracking-tight leading-[115%] text-[2.7rem] sm:text-7xl lg:text-[5rem] xl:text-[7.2rem]">
+            <h2 className="font-display text-foreground text-balance mx-auto  max-w-5xl text-center font-medium tracking-tight leading-[115%] text-[2.4rem] sm:text-6xl lg:text-[5rem] xl:text-[7.2rem]">
               Manufacturing software{" "}
               <span className="text-secondary">made easy</span>
             </h2>
