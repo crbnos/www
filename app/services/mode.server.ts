90a3667830888eef376f6b32b07e724fbeb0e179
Brad Barbin <bradbarbin@protonmail.com>
2026-03-30 12:43:49 -0400

    fix: cookie mode

--- app/services/mode.server.ts
+++ app/services/mode.server.ts
@@ -4,21 +4,28 @@ import type { Mode } from "~/types/validators";
 const cookieName = "mode";
 
 export function getMode(request: Request, hint?: Mode): Mode | null {
-  const cookieHeader = request.headers.get("cookie");
-  const parsed = cookieHeader
-    ? cookie.parse(cookieHeader)[cookieName]
-    : "light";
-  if (parsed === "light" || parsed === "dark") return parsed;
-  return hint ?? null;
+	const cookieHeader = request.headers.get("cookie");
+	const parsed = cookieHeader
+		? cookie.parse(cookieHeader)[cookieName]
+		: "light";
+	if (parsed === "light" || parsed === "dark") return parsed;
+	return hint ?? null;
 }
 
 export function setMode(mode: Mode | "system") {
-  if (mode === "system") {
-    return cookie.serialize(cookieName, "", { path: "/", maxAge: -1 });
-  } else {
-    return cookie.serialize(cookieName, mode, {
-      path: "/",
-      maxAge: 31536000,
-    });
-  }
+	if (mode === "system") {
+		return cookie.serialize(cookieName, "", {
+			domain:
+				process.env.VERCEL_ENV === "production" ? ".carbon.ms" : undefined,
+			path: "/",
+			maxAge: -1,
+		});
+	} else {
+		return cookie.serialize(cookieName, mode, {
+			domain:
+				process.env.VERCEL_ENV === "production" ? ".carbon.ms" : undefined,
+			path: "/",
+			maxAge: 31536000,
+		});
+	}
 }
