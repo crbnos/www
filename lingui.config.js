1ef60764175b4226d4f918f4e64ff0eb61c3a1c1
Brad Barbin <bradbarbin@protonmail.com>
2026-04-11 01:11:59 -0400

    fix: fallback language for missing translations

--- lingui.config.js
+++ lingui.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from "@lingui/cli";
 
 export default defineConfig({
-  sourceLocale: "en",
-  locales: ["en", "es", "de", "it", "ja", "zh"],
-  format: "po",
-  catalogs: [
-    {
-      path: "locales/{locale}/www",
-      include: ["app"],
-      exclude: ["**/*.server.*", "**/*.test.*", "**/*.spec.*"],
-    },
-  ],
+	sourceLocale: "en",
+	locales: ["en", "es", "de", "it", "ja", "zh"],
+	fallbackLocales: {
+		default: "en",
+	},
+	format: "po",
+	catalogs: [
+		{
+			path: "locales/{locale}/www",
+			include: ["app"],
+			exclude: ["**/*.server.*", "**/*.test.*", "**/*.spec.*"],
+		},
+	],
 });
